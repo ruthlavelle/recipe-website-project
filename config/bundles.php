12e9c3ec281d41421f0b6a8e6262b0963230d920
ruthlavelle <b00100335@student.itb.ie>
2018-05-13 00:25:39 +0100

    commented out debug bundle for Heroku

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,7 @@ return [
     //Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    //Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     //Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
